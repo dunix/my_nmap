42711f40f59dc516f00ff88638f4ba50e6733557
dunix <josdav.ati@gmail.com>
2014-03-17 13:52:14 -0600

    info

--- source/my_nmap.py
+++ source/my_nmap.py
@@ -1,14 +1,23 @@
-from scapy.all import *
-import sys
 
-def revisarEntrada():	
+""" ITCR - IC8042
+ David Chaverri Perez
+ Tarea corta 4: 
+ Python usando libreria scapy para emular algunos comandos de nmap
+"""
+
+from scapy.all import * #libreria scapy
+import sys 				#entrada estandar del terminal (utilidad sys)
+
+def revisarEntrada():
+	#función para revisar los parámetros de entrada
 	if len(sys.argv) != 2:
-		print "----Falta dirección IP-----"
+		print ("----Error: Falta argumento Dirección IP-----")
 		sys.exit(1)
 		
 def main():
 	revisarEntrada()
-	print (sys.argv[1])
+	print ("dirección IP: "+ sys.argv[1])
+	
 
 main()
 	
