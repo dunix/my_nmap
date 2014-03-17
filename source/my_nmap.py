b9bc8a2bb80207af6bfc0ca33e2c2d3bcc52271d
dunix <josdav.ati@gmail.com>
2014-03-17 15:16:15 -0600

    opciones ayuda

--- source/my_nmap.py
+++ source/my_nmap.py
@@ -8,17 +8,56 @@
 from scapy.all import * #libreria scapy
 import sys 				#entrada estandar del terminal (utilidad sys)
 
+def ayuda():
+	print ("ayuda en mantenimiento")
+	sys.exit(1)
+
 def revisarEntrada():
 	#función para revisar los parámetros de entrada
-	if len(sys.argv) != 2:
-		print ("----Error: Falta argumento Dirección IP-----")
+	if len(sys.argv) != 3:
+		if sys.argv[1] == "-h":
+			ayuda()
+		else:
+			print ("Error - Faltan parámetros")
+			print ("---help---")
+			print ("Para ayuda digite: my_nmap -h")
+			sys.exit(1)
+			
+def TCP_Connect():
+	print("TCP_Connect")
+		
+def opciones():
+	if sys.argv[1] == "-sT":
+		return TCP_Connect()
+	else:
+		print ("Error - opción inválida")
+		print ("---help---")
+		print ("Para ayuda digite:my_nmap -h")
 		sys.exit(1)
+	
+"""		
+	if sys.argv[1] == "-sS":
+		return TCP_SYN()
+	if sys.argv[1] == "-sA":
+		return TCP_ACK()			
+	if sys.argv[1] == "-sF":
+		return TCP_FIN()		
+	if sys.argv[1] == "-sI":
+		return TCP_IDLE_SCAN()		
+	if sys.argv[1] == "-sU":
+		return TCP_UDP()			
+"""
+
+	
+			
 		
 def main():
 	revisarEntrada()
-	print ("dirección IP: "+ sys.argv[1])
+	opciones()
+
+
 	
 
-main()
+main() #llamado a función main()
 	
 
